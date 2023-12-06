dab11f387c6d5bdcacdced846b92a0a77b7c8ec1
Fozzyack <fsundra@gmail.com>
2023-12-07 01:58:30 +0800

    fixing links

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -23,7 +23,7 @@ const Socials = () => {
                     <p>TikTok</p>
                 </a>
 
-                <a className='flex flex-col items-center font-bold text-white' href='https://www.twitch.tv/higuvt/about'>
+                <a className='flex flex-col items-center font-bold text-white' href='https://throne.com/higuvt'>
                 <svg className='w-10 h-10 -mt-4' fill="#FFFFFF" width="800px" height="800px" viewBox="0 0 1000 1000" version="1.1" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 1000 1000">
 
                     <g>
@@ -62,7 +62,7 @@ const Socials = () => {
             </a>
 
             </div>
-            <a className='flex flex-col items-center font-bold text-white p-3 bg-purple-800 rounded-xl mt-5' href='https://www.twitch.tv/higuvt/about'>
+            <a className='flex flex-col items-center font-bold text-white p-3 bg-purple-800 rounded-xl mt-5' href='https://www.twitch.tv/higuvt/'>
                     <svg className='w-6 h-6' fill='#FFFFFF' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M2.149 0l-1.612 4.119v16.836h5.731v3.045h3.224l3.045-3.045h4.657l6.269-6.269v-14.686h-21.314zm19.164 13.612l-3.582 3.582h-5.731l-3.045 3.045v-3.045h-4.836v-15.045h17.194v11.463zm-3.582-7.343v6.262h-2.149v-6.262h2.149zm-5.731 0v6.262h-2.149v-6.262h2.149z" fill-rule="evenodd" clip-rule="evenodd" /></svg>
                     <p>Twitch</p>
                 </a>
